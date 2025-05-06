868d952d158ed1553ffed5eaec16aa332dd4b06e
Brian778812 <bo63737n@pace.edu>
2025-05-06 16:38:02 -0400

    Update security.sql

--- security.sql
+++ security.sql
@@ -4,8 +4,8 @@ CREATE ROLE readonly;
 CREATE USER user1 WITH PASSWORD 'Password1';
 CREATE USER user2 WITH PASSWORD 'Password2';
 
-GRANT readonly TO user1;
-GRANT readonly TO user2;
+GRANT readonly TO user4;
+GRANT readonly TO user5;
 
 GRANT CONNECT ON DATABASE your_database_name TO readonly;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO readonly;
