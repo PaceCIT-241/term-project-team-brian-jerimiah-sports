05dec76d001643f95cc14b2887ee7c6a829e75b2
Brian778812 <bo63737n@pace.edu>
2025-05-06 16:37:45 -0400

    Add files via upload

---
+++ security.sql
@@ -0,0 +1,11 @@
+-- security.sql
+CREATE ROLE readonly;
+
+CREATE USER user1 WITH PASSWORD 'Password1';
+CREATE USER user2 WITH PASSWORD 'Password2';
+
+GRANT readonly TO user1;
+GRANT readonly TO user2;
+
+GRANT CONNECT ON DATABASE your_database_name TO readonly;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO readonly;
